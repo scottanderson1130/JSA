1dee09421f57f90b42557fe67213363ef746d584
Scott Anderson <scott.anderson1130@gmail.com>
2020-11-30 17:14:19 -0600

    (Setup) server created

---
+++ server/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+const DIST_DIR = path.join(__dirname, '../client');
+const HTML_FILE = path.join(DIST_DIR, 'index.html');
+app.use(express.static(DIST_DIR));
+
+app.listen(1130, () => {
+  console.log(`listening on http://localhost:1130`)
+});
+
