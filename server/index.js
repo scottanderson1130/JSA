6352c7571e074c60bfd95a7ba9bc2da6fe9e6736
Scott Anderson <scott.anderson1130@gmail.com>
2020-12-10 10:52:32 -0600

    (update) created env variables#

--- server/index.js
+++ server/index.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const path = require('path');
+require('dotenv').config();
+
+const PORT = process.env.PORT || 8080;
 
 const app = express();
 
@@ -7,6 +10,6 @@ const DIST_DIR = path.join(__dirname, '../dist');
 const HTML_FILE = path.join(DIST_DIR, 'index.html');
 app.use(express.static(DIST_DIR));
 
-app.listen(1130, () => {
-  console.log(`listening on http://localhost:1130`);
+app.listen(PORT, () => {
+  console.log(`listening on http://localhost:${PORT}`);
 });
